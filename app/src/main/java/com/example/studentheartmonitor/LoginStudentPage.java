96de1b20d06dce803caa0e99951efd73489be5cb
Solomonso <solomonn135@gmail.com>
2019-12-02 18:31:49 +0100

    Created the login page for students. Make some adjustment to it later. Will start validating the inputs next.

---
+++ app/src/main/java/com/example/studentheartmonitor/LoginStudentPage.java
@@ -0,0 +1,14 @@
+package com.example.studentheartmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginStudentPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_student_page);
+    }
+}
--- app/src/main/java/com/example/studentheartmonitor/MainActivity.java
+++ app/src/main/java/com/example/studentheartmonitor/MainActivity.java
@@ -11,22 +11,42 @@ import com.example.studentheartmonitor.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private Button buttonStudent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonStudent = findViewById(R.id.buttonStudent);
+        buttonStudent.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openStudentLoginPage();
+            }
+        });
+
+        //teacher buttons
         button = findViewById(R.id.buttonTeacher);
         button.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                openLoginActivity();
+                openTeacherLoginPage();
             }
         });
     }
-    public void openLoginActivity()
+    //student
+    public void openStudentLoginPage()
+    {
+        Intent intent = new Intent(this, LoginStudentPage.class);
+        startActivity(intent);
+    }
+
+    //teacher
+    public void openTeacherLoginPage()
     {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
