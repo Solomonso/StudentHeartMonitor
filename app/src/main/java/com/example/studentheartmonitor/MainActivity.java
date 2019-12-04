dd5aa1c1109aad4890c4293821690e7fb5c5ce31
Solomonso <solomonn135@gmail.com>
2019-12-04 15:54:08 +0100

    added comment

--- app/src/main/java/com/example/studentheartmonitor/MainActivity.java
+++ app/src/main/java/com/example/studentheartmonitor/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 openTeacherLoginPage();
+                //added comment
             }
         });
     }
