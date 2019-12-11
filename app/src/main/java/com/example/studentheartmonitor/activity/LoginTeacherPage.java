b59079a12608dfd6432bde62a9049f47942f45d0
Solomonso <solomonn135@gmail.com>
2019-12-11 20:01:40 +0100

    Designed the teacher interface. Will start working on how to create lesson and join the lesson next.

--- app/src/main/java/com/example/studentheartmonitor/activity/LoginTeacherPage.java
+++ app/src/main/java/com/example/studentheartmonitor/activity/LoginTeacherPage.java
@@ -2,6 +2,7 @@ package com.example.studentheartmonitor.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
--- app/src/main/java/com/example/studentheartmonitor/activity/TeacherActivity.java
+++ app/src/main/java/com/example/studentheartmonitor/activity/TeacherActivity.java
@@ -1,27 +1,46 @@
 package com.example.studentheartmonitor.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.time.format.DateTimeFormatter;
+
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 
 
 import com.example.studentheartmonitor.MainActivity;
 import com.example.studentheartmonitor.R;
 import com.example.studentheartmonitor.helper.SQLiteHandler;
 import com.example.studentheartmonitor.helper.SessionManager;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class TeacherActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
     private TextView txtName;
     private TextView txtEmail;
-    private Button btnLogout;
 
     private SQLiteHandler db;
     private SessionManager session;
@@ -31,9 +50,56 @@ public class TeacherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
 
+        //toolbar
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        //adding the action bar
+        final ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+
+
+        //menus in the navigation
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationView);
+
+        //get the navigation items selected on the menu toolbar
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_lesson_overview:
+                        menuItem.setChecked(true);
+                        displayMessage("Lesson overview Selected...");
+                        drawerLayout.closeDrawers();
+                        return true;
+
+                    case R.id.nav_log_out:
+                        menuItem.setChecked(true);
+                        logoutUser();
+                        drawerLayout.closeDrawers();
+                        return true;
+                }
+                return false;
+            }
+        });
+        //store date details
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        //to display the date
+        TextView textViewDate = findViewById(R.id.text_view_date);
+        textViewDate.setText(currentDate);
+
+        //get the name and email by id
         txtName = findViewById(R.id.name);
         txtEmail =  findViewById(R.id.email);
-        btnLogout =  findViewById(R.id.btnLogout);
 
         // SqLite database handler
         db = new SQLiteHandler(getApplicationContext());
@@ -55,14 +121,25 @@ public class TeacherActivity extends AppCompatActivity {
         txtName.setText(name);
         txtEmail.setText(email);
 
-        // Logout button click event
-        btnLogout.setOnClickListener(new View.OnClickListener() {
+    }
 
-            @Override
-            public void onClick(View v) {
-                logoutUser();
-            }
-        });
+    //method to make the action bar clickable
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                        drawerLayout.openDrawer(GravityCompat.START);
+                        return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //for testing. to display the current item clicked on the menu bar
+    private void displayMessage(String message)
+    {
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
     }
 
     /**
