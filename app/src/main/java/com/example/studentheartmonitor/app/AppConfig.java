18885a951912fceeb30d9fa6c7ffd4d47f92b594
VanThuyVu <47418855+VanThuyVu@users.noreply.github.com>
2019-12-18 11:52:40 +0100

    Student interface

--- app/src/main/java/com/example/studentheartmonitor/app/AppConfig.java
+++ app/src/main/java/com/example/studentheartmonitor/app/AppConfig.java
@@ -2,7 +2,7 @@ package com.example.studentheartmonitor.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.178.17/android_login_api/login.php";
+    public static String URL_LOGIN = "http://10.232.36.154:80/android_login_api/login.php";
     // Server user register url
     public static String URL_REGISTER = "http://192.168.178.17/android_login_api/register.php";
 }
