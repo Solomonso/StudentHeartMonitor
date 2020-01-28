7ccbdab11ff1b063fb5cb28b508248b76889ed71
Solomonso <solomonn135@gmail.com>
2020-01-28 11:46:22 +0100

    updated api

--- heartrate_api/sendBPM.php
+++ heartrate_api/sendBPM.php
@@ -1,25 +1,24 @@
 <?php
-	
  require_once "include/DB_Functions.php";
  $db = new DB_Functions();
 
  $response = array("error"=>FALSE);
- if(isset($_POST["StringBPM"]))
+ if(isset($_POST["heartrate"]))
  {
- 	$StringBPM = $_POST["StringBPM"];
+ 	$heartrate = $_POST["heartrate"];
 
- 	$send = $db->sendBPM($StringBPM);
+ 	$send = $db->sendBPM($heartrate);
 
  	if($send)
  	{
  		$response["error"] = FALSE;
- 		$response["send"]["StringBPM"] = $send["StringBPM"];
+ 		$response["send"]["heartrate"] = $send["heartrate"];
  		echo json_encode($response);
  	}
  	else
 	{
         $response["error"] = TRUE;
-        $response["error_msg"] = "Unknown error occurred in sending. Please try again!";
+        $response["error_msg"] = "Unable to send BPM. Please try again!";
         echo json_encode($response);
 	}
  }
